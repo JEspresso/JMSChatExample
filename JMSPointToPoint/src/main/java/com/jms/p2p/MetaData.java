1f15e813b6e57eb5c8b862eb1534386d47f5d788
Kevin <Kevin@DESKTOP-NGL3H3B>
2017-12-20 02:02:50 +0300

    Metadata about point-to-point JMS connection

---
+++ JMSPointToPoint/src/main/java/com/jms/p2p/MetaData.java
@@ -0,0 +1,35 @@
+package com.jms.p2p;
+
+import java.util.Enumeration;
+import javax.jms.ConnectionMetaData;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class MetaData {
+	
+	public static void main(String args[]) {
+		try {
+			Context context = new InitialContext();
+			QueueConnectionFactory queueConnectionFactory = (QueueConnectionFactory)context.lookup("QueueCF");
+			QueueConnection queueConnection = queueConnectionFactory.createQueueConnection();
+			ConnectionMetaData connectionMetaData = queueConnection.getMetaData();
+			
+			System.out.println("JMS Version: "  + connectionMetaData.getJMSMajorVersion() + "." + connectionMetaData.getJMSMinorVersion());
+			System.out.println("JMS Provider: " + connectionMetaData.getJMSProviderName());
+			System.out.println("JMSX Properties Supported: ");
+			
+			@SuppressWarnings("rawtypes")
+			Enumeration enumeration = connectionMetaData.getJMSXPropertyNames();
+			
+			while(enumeration.hasMoreElements()) {
+				System.out.println("\t-" + enumeration.nextElement());
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+}
