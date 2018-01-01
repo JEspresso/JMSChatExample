342a7adc9d8ec381f983a337eca6c2dd4728c516
Kevin <Kevin@DESKTOP-NGL3H3B>
2018-01-01 22:16:26 +0300

    Additional comments on QLender class

--- JMSPointToPoint/src/main/java/com/jms/p2p/QLender.java
+++ JMSPointToPoint/src/main/java/com/jms/p2p/QLender.java
@@ -4,7 +4,15 @@ import java.io.*;
 import javax.jms.*;
 import javax.naming.*;
 
-/*	The role of the QLender class is to listen for loan requests
+/*	The role of the QLender class is to listen for loan requests on the loan request queue, determine if the salary meets the necessary business 
+ * 	requirements, and finally send back the results to the borrower. 
+ * 
+ * 	In this example, QLender class is referred to as a "message listener" and, as such, implements the javax.jms.MessageListener interface and 
+ * 	overrides the onMessage() method. 
+ * 
+ * 	QLender class is an asynchronous message listener, meaning that unlike the QBorrower it will not block when waiting for messages. 
+ * 
+ * 	This is evident from the fact the QLender class implements the MessageListener interface and then overrides the onMessage() method. 
  * */
 
 public class QLender implements MessageListener {
@@ -13,9 +21,11 @@ public class QLender implements MessageListener {
 	private QueueSession qSession = null;
 	private Queue requestQ = null;
 
+	
+	//	The constructor in the QLender class works in the same way as the constructor in the QBorrower class 
 	public QLender(String queueCF, String requestQueue) {
 		try {
-			// connect to the provider and get the JMS connection
+			//	Establish a connection to the provider, do a JNDI lookup to get the queue, creates a QueueSession and then starts the connection
 			Context context = new InitialContext();
 			QueueConnectionFactory queueConnectionFactory = (QueueConnectionFactory) context.lookup(queueCF);
 			qConnect = queueConnectionFactory.createQueueConnection();
@@ -29,10 +39,21 @@ public class QLender implements MessageListener {
 			// now that the setup is complete, start the connection
 			qConnect.start();
 
-			// create the message listener
+			/*	Once the connection is started, the QLender class can beging to receive messages
+			 * 	However, before it can receive messages, it must be registered by the QueueReceiver as a message listener */
 			QueueReceiver queueReceiver = qSession.createReceiver(requestQ);
+			
+			/*	At this point, a separate listener thread is started. That thread will wait until a message is received, and upon receipt of a 
+			 * 	message, will invoke the onMessage() method of the listener class. 
+			 * 
+			 * 	In this case, we set the message to the QLender class using the "this" keyword in the setMessageListner() method. 
+			 * 
+			 * 	We could have easily delegated the messaging work to another class that implemented the MessageListener interface
+			 * 	
+			 * 	queueReceiver.setMessageListener(someOtherClass);
+			 * 
+			 * */
 			queueReceiver.setMessageListener(this);
-
 			System.out.println("Waiting for loan requests...");
 		} catch (JMSException ex) {
 			ex.printStackTrace();
@@ -44,16 +65,22 @@ public class QLender implements MessageListener {
 	}
 
 	public void onMessage(Message message) {
+		
+		/*	When a message is received on the queue specified in the createReceiver() method, the listener thread will asynchronously invoke 
+		 * 	the onMessage() method of the listener class (in our case, the QLender class is also the listener class) */
+		
 		boolean accepted = false;
 		
 		if (message instanceof MapMessage) {
 			try {
-				// get the data from the message
+				/* 	The onMessage() method first casts the Message to a MapMessage (the message type we are expecting to receive from the borrower 
+				 * 	
+				 * 	It then extracts the salary and loan amount requested from the message payload, checks the salary to loan amount ratio, then 
+				 * 	determines whether to accept or decline the loan request */
 				MapMessage mapMessage = (MapMessage) message;
 				double salary = mapMessage.getDouble("Salary");
 				double loanAmount = mapMessage.getDouble("Loan Amount");
 
-				// determine whether to accept or decline the loan request
 				if (loanAmount < 200000) {
 					accepted = (salary / loanAmount) > .25;
 				} else {
@@ -61,12 +88,36 @@ public class QLender implements MessageListener {
 				}
 				System.out.println("" + "Percent = " + (salary / loanAmount) + ", loan is " + (accepted ? "Accepted!" : "Declined"));
 
-				// send the result back to the borrower
+				/* 	Again, to make this more failsafe, it would be better to check the JMS message type using the instanceof operator in the even that 
+				 * 	another message type was being sent to that queue 
+				
+					if(mapMessage instanceof MapMessage) {
+						//	process the request
+						} else {
+							throw new IllegalArgumentException("Unsupported message type");
+						} 	*/
+				
+				/* 	Once the loan request has been analyzed and the results determined, the QLender class needs to send the response back to the 
+				 *  borrower. 
+				 *  
+				 *  It does this by first creating a JMS message to send. The response message does not need to be the same JMS message type as the 
+				 *  loan request message that was received by the QLender. 
+				 *  
+				 *  To illustrate this point, the QLender returns a TextMessage back to the QBorrower. */
 				TextMessage textMessage = qSession.createTextMessage();
 				textMessage.setText(accepted ? "Accepted!" : "Declined");
+				
+				/*	The next statement sets the JMSCorrelationID, which is the JMS header property that is used by the QBorrower class to filter the 
+				 * 	incoming response messages */
+				
 				textMessage.setJMSCorrelationID(mapMessage.getJMSCorrelationID());
 
-				// create the sender and send the message
+				/*	Once the message is created, the onMessage() method then sends the message to the response queue specified by the JMSReplyTo 
+				 * 	message header property. 
+				 * 
+				 * 	In the QBorrower class we set the JMSReplyTo header property when sending the original loan request. 
+				 * 
+				 * 	The QLender class can now use that header that property as the destination to send the response message to */
 				QueueSender queueSender = qSession.createSender((Queue) mapMessage.getJMSReplyTo());
 				queueSender.send(textMessage);
 
@@ -90,6 +141,10 @@ public class QLender implements MessageListener {
 		System.exit(0);
 	}
 
+	/*	Main method validates the command-line arguments and then invokes the constructor by instantiating a new QLender class. 
+	 * 
+	 * 	It then keeps the primary thread alive until the enter key is pressed on the command line
+	 * */
 	public static void main(String args[]) {
 		String queueCF = null;
 		String requestQ = null;
